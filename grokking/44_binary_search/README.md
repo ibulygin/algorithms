6e32ae611faf0a7c19280ab4f03b0cdc08a53cb3
Alexander Loktionov <loktionov129@gmail.com>
2018-10-05 20:46:02 +0700

    feat(grokking): recursive binary_search
    In its simplest form, binary search is used to quickly find a value in a sorted sequence.
    
    refs: #04

---
+++ grokking/44_binary_search/README.md
@@ -0,0 +1,30 @@
+# grokking
+## 01_binary_search
+
+### Run
+
+```sh
+$ make run
+python main.py
+numbers: [32, 36, 37, 38, 39, 41, 42, 43, 44]
+x: 33
+result: None
+
+$ make run
+python main.py
+numbers: [31, 32, 33, 36, 38, 40, 41]
+x: 40
+result: 5
+```
+
+### Test
+
+```sh
+$ make test
+python -m unittest algorithms_tests.py
+...........
+----------------------------------------------------------------------
+Ran 11 tests in 0.002s
+
+OK
+```
---
+++ grokking/44_binary_search/algorithms.py
@@ -0,0 +1,30 @@
+from typing import List
+
+def binary_search(numbers: List[int], x: int) -> int:
+    numbersCount = len(numbers)
+
+    if numbersCount == 0:
+        return None
+    
+    if numbersCount == 1:
+        return 0 if numbers[0] == x else None
+
+    if numbers[0] == x:
+        return 0
+
+    if numbers[numbersCount - 1] == x:
+        return numbersCount - 1
+    
+    mid = (numbersCount - 1) // 2
+
+    if numbers[mid] > x:
+        return binary_search(numbers[0:mid], x)
+    else:
+        if mid == 0:
+            return None
+
+        result = binary_search(numbers[mid:], x)
+        if result == None:
+            return None
+        else:
+            return mid + result
